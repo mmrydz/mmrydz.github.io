61e0a98644c37343c9ad75814b50d61ec83a9ce6
mmrydz <megrydzewski@gmail.com>
2019-06-03 09:04:18 -0400

    added links below projects to view code

--- src/Components/portfolio/index.js
+++ src/Components/portfolio/index.js
@@ -16,11 +16,13 @@ class Portfolio extends Component {
                   <div className="portfolio-item-meta">
                  <h5>{projects.title}</h5>
                      <p>{projects.category}</p>
+
                   </div>
                 </div>
               <div className="link-icon"><i className="fa fa-link"></i></div>
             </a>
           </div>
+          <a href={projects.github} className="github-link" target="_blank">View the code on GitHub.</a>
         </div>
       })
     }
